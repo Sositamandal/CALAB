2caa166c3e15a7adc612273a16c2bd5378bab20c
Sositamandal <sositamandal18@gmail.com>
2026-07-03 11:29:18 +0545

    Add files via upload

---
+++ lab4/demux_tb.vhd
@@ -0,0 +1,47 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity DEMUX_TB is
+end entity DEMUX_TB;
+
+architecture Simulation of DEMUX_TB is
+
+    signal D : STD_LOGIC := '1';
+    signal S : STD_LOGIC_VECTOR(1 downto 0) := "00";
+    signal Y : STD_LOGIC_VECTOR(3 downto 0);
+
+begin
+
+    -- Instantiate the DEMUX
+    DUT : entity work.DEMUX_1TO4
+        port map (
+            D => D,
+            S => S,
+            Y => Y
+        );
+
+    -- Apply test vectors
+    STIMULUS : process
+    begin
+        D <= '1';
+
+        S <= "00";
+        wait for 10 ns;   -- Expected Y = 0001
+
+        S <= "01";
+        wait for 10 ns;   -- Expected Y = 0010
+
+        S <= "10";
+        wait for 10 ns;   -- Expected Y = 0100
+
+        S <= "11";
+        wait for 10 ns;   -- Expected Y = 1000
+
+        D <= '0';
+        S <= "10";
+        wait for 10 ns;   -- Expected Y = 0000
+
+        wait;
+    end process;
+
+end architecture Simulation;
